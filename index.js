99e45ea5ab46c7d262658f3cd1d1e4dadfce0d87
trisdepzai <tritocao@gmail.com>
2024-05-26 02:11:43 +0700

    [26-5-2024] First commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// packages
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+// Utiles
+import connectDB from "./src/config/db.js";
+import userRoutes from "./src/routes/userRoutes.js";
+import categoryRoutes from "./src/routes/categoryRoutes.js";
+import productRoutes from "./src/routes/productRoutes.js";
+import uploadRoutes from "./src/routes/uploadRoutes.js";
+import orderRoutes from "./src/routes/orderRoutes.js";
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/orders", orderRoutes);
+
+app.get("/api/config/paypal", (req, res) => {
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
+});
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+
+
+setTimeout(async ()=>{
+  await connectDB();
+  app.listen(port, () => console.log(`Server running on port: ${port}`));
+})
