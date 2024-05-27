ee220d9cd9df2f6320355b3d95ee3371fd39fc5a
trisdepzai <tritocao@gmail.com>
2024-05-28 05:53:59 +0700

    [28-5-2024] Third commit

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 
-const app = express();
+const app = express()
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
