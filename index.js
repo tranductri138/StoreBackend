f90bcb6a4a119b5ffcd12d05284f45f891dc706e
tranductri <tritd@foobla.com>
2024-05-31 11:15:45 +0700

    [31-5-2024] Update code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import morgan from "morgan";
 
 // Utiles
 import connectDB from "./src/config/db.js";
@@ -13,16 +14,20 @@ import uploadRoutes from "./src/routes/uploadRoutes.js";
 import orderRoutes from "./src/routes/orderRoutes.js";
 
 import { isExistFile } from "./src/service/File.js"
+import fs from "fs";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 
 const app = express()
+const __dirname = path.resolve()
+
+app.use(morgan('default'))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser());
+app.use(cookieParser())
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
@@ -34,11 +39,14 @@ app.get("/api/config/paypal", (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 
-const __dirname = path.resolve();
+app.get("/hello" , (req , res) => {
+  return res.send({ message :"Hi im store-backend"})
+})
+
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 setTimeout(async ()=>{
   isExistFile(`${__dirname}/uploads`)
-  await connectDB();
+  // await connectDB();
   app.listen(port, () => console.log(`Server running on port: ${port}`));
 })
